dcfb5c03dc419d99f95d0b2592a2d25e4866a480
chenlunTian <354769733@qq.com>
2020-02-17 20:15:54 +0800

    STC15资料

---
+++ STC15/Lib/int.h
@@ -0,0 +1,8 @@
+#ifndef __INT_H__
+#define __INT_H__
+
+#include "headfile.h"
+
+void INT_Init(); 
+
+#endif
---
+++ STC15/Lib/timer.h
@@ -0,0 +1,8 @@
+#ifndef __TIMER_H__
+#define __TIMER_H__
+
+#include "headfile.h"
+
+void Timer_Init(unsigned int T_N100us);
+
+#endif
